a75ebbd29655a241901fe25434ebfa9869ae6210
mengliang <13767029108@163.com>
2023-09-06 13:09:58 +0800

    初始版导航栏及logo

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="globalHeader">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/CYlogo.png" />
+          <div class="title">Online编程</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">Solution</a-menu-item>
+      <a-menu-item key="3">Cloud Service</a-menu-item>
+      <a-menu-item key="4">Cooperation</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title-bar {
+  align-items: center;
+}
+
+.title {
+  color: #444;
+}
+
+.logo {
+  height: 48px;
+}
+</style>
--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,5 +1,49 @@
 <template>
-  <router-view />
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://yupi.icu" target="_blank"> 创耘科技 by 熊磊 </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
 </template>
 
-<style></style>
+<style scoped>
+#basicLayout {
+}
+
+#basicLayout .header {
+  background: red;
+  margin-bottom: 16px;
+  box-shadow: #eee 1px 1px 5px;
+}
+
+#basicLayout .content {
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 16px;
+  padding: 20px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: sticky;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
