864c13422a3fc0b79aba078647adc89adea90227
mengliang <13767029108@163.com>
2023-09-06 19:31:45 +0800

    根据点击路由跳转到对应的页面

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="globalHeader">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
+    <a-menu
+      mode="horizontal"
+      :default-selected-keys="['1']"
+      @menu-item-click="doMenuClick"
+    >
       <a-menu-item
         key="0"
         :style="{ padding: 0, marginRight: '38px' }"
@@ -11,15 +15,25 @@
           <div class="title">Online编程</div>
         </div>
       </a-menu-item>
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">Solution</a-menu-item>
-      <a-menu-item key="3">Cloud Service</a-menu-item>
-      <a-menu-item key="4">Cooperation</a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path">
+        {{ item.name }}
+      </a-menu-item>
     </a-menu>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "../router/routes";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
