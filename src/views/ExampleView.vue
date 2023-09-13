4f7fb4075e65bcf4442d60e671f2f817c26ab39f
mengliang <13767029108@163.com>
2023-09-13 08:54:20 +0800

    创建题目页面

---
+++ src/views/ExampleView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exampleView">
+    <MdEditor :value="value" :handle-change="onChange" />
+    <CodeEditor />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import MdEditor from "@/components/MdEditor.vue";
+import CodeEditor from "@/components/CodeEditor.vue"; // @ is an alias to /src
+
+const value = ref();
+const onChange = (v: string) => {
+  value.value = v;
+};
+</script>
