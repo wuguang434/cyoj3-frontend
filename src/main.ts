84ae1f25655edebbbefcfd9fb514441f4e176c42
mengliang <13767029108@163.com>
2023-09-13 08:53:00 +0800

    代码编辑器页面

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,6 @@ import router from "./router";
 import store from "./store";
 import "@/plugins/axios";
 import "@/access";
+import "bytemd/dist/index.css";
 
 createApp(App).use(ArcoVue).use(router).use(store).mount("#app");
