e76cc061c4a702befb168549d776e7bf9086b1f3
mengliang <13767029108@163.com>
2023-10-14 13:43:04 +0800

    增加已提交页面路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -9,6 +9,7 @@ import UserRegisterView from "@/views/user/UserRegisterView.vue";
 import AddQuestionView from "@/views/question/AddQuestionView.vue";
 import ManageQuestionView from "@/views/question/ManageQuestionView.vue";
 import QuestionsView from "@/views/question/QuestionsView.vue";
+import QuestionSubmitView from "@/views/question/QuestionSubmitView.vue";
 import ViewQuestionView from "@/views/question/ViewQuestionView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
@@ -33,9 +34,14 @@ export const routes: Array<RouteRecordRaw> = [
     },
   },
   {
-    path: "/",
+    path: "/questions",
     name: "浏览题目",
     component: QuestionsView,
+  },
+  {
+    path: "/question_submit",
+    name: "已提交页面",
+    component: QuestionSubmitView,
   },
   {
     path: "/view/question/:id",
